782a5f72a32896d6ec0f316605c1081a47cfc3f6
Cleuton Barreto <cleuton.barreto@gmail.com>
2023-10-11 21:26:39 -0300

    Backend project created

---
+++ Fruttare/src/main/java/com/fruttare/app/FruttareApplication.java
@@ -0,0 +1,13 @@
+package com.fruttare.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruttareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruttareApplication.class, args);
+	}
+
+}
---
+++ Fruttare/src/test/java/com/fruttare/app/FruttareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fruttare.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruttareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
