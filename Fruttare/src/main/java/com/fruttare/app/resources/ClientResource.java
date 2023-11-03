a807cc2d55ac9cc42e31a7fef28042ee5e4f0dce
Cleuton Barreto <cleuton.barreto@gmail.com>
2023-11-03 00:36:05 -0300

    ClienteDTO

--- Fruttare/src/main/java/com/fruttare/app/resources/ClientResource.java
+++ Fruttare/src/main/java/com/fruttare/app/resources/ClientResource.java
@@ -1,2 +1,32 @@
-package com.fruttare.app.resources;public class ClientResource {
+package com.fruttare.app.resources;
+
+import com.fruttare.app.dto.ClientDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClientResource {
+
+//    private ClientService service;
+
+@RequestMapping("/")
+public ModelAndView index(){
+   return new ModelAndView("index");
+    }
+
+    @GetMapping
+    public ResponseEntity <List<ClientDTO>> findAll(){
+        List<ClientDTO> list = new ArrayList<>();
+        list.add(new ClientDTO(35L, "Joseph", "15342563250", 4500.0, Instant.parse("1996-12-16T08:25:00Z"),2  ));
+        return ResponseEntity.ok().body(list);
+    }
+
 }
